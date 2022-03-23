763ef89c2b502e3e4fcd63c395f18eda73352fef
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2022-03-23 15:12:14 -0300

    fixing metric url

--- js/utils/bindStreamMetrics.js
+++ js/utils/bindStreamMetrics.js
@@ -1,10 +1,11 @@
 export const bindStreamMetrics = (streammetricsOptions, videoElement, metricsHost) => {
   if(streammetricsOptions){
     try{
-      let metricsDir
-      const streammetrics     = new window['StreamMetrics']['default']("https://" + metricsHost + "/inbound/v1/event/register")
+      let metricsURL = metricsHost.includes('http') ? metricsHost : "https://" + metricHost + "/inbound/v1/event/register"
+      const streammetrics     = new window['StreamMetrics']['default']( metricsURL)
       const streammetricsApi  = streammetrics['wrap'](videoElement)
       let streamMetricsConfig = Object.assign({'api': streammetricsApi}, streammetricsOptions)
+      console.log('streammetricsOptions',streammetricsOptions)
       streammetrics['start'](streamMetricsConfig)
     } catch (e) {
       console.log(e)
